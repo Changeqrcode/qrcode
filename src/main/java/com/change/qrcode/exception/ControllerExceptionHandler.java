712231dc981508e9dd7fce6a98fb43b2edb31e51
Murat Yildiz <yildiz.murat@tubitak.gov.tr>
2023-02-17 00:35:40 +0300

    v 13
    UI changes

--- src/main/java/com/change/qrcode/exception/ControllerExceptionHandler.java
+++ src/main/java/com/change/qrcode/exception/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
@@ -22,22 +23,35 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ModelAndView globalExceptionHandler(Exception ex, WebRequest request ){
         items = ex.getClass().getName().split("\\.");
-
-        ErrorMessage message = new ErrorMessage(
-                items[items.length-1].substring(0, 1).toLowerCase() + items[items.length-1].substring(1),
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                new Date(),
-                ex.getMessage(),
-                request.getDescription(true),
-                ex.getStackTrace().toString(),
-                ex.getLocalizedMessage(),
-                ex.getCause().toString());
-
-        errorMessageRepository.saveAndFlush(message);
-
         ModelAndView m = new ModelAndView();
-        m.setViewName("error");
-        m.addObject("errorMessage", message);
+
+        if(ex instanceof MethodArgumentTypeMismatchException){
+            ErrorMessage message = new ErrorMessage(items[items.length-1].substring(0, 1).toLowerCase() + items[items.length-1].substring(1),
+                                                    HttpStatus.NO_CONTENT.value(),
+                                                    new Date(),
+                                            "Gitmeye çalıştığınız link bozuk olabilir veya böyle bir link olmayabilir.",
+                                                    request.getDescription(true),
+                                                    ex.getStackTrace().toString(),
+                                                    ex.getLocalizedMessage(),
+                                                    ex.getCause().toString());
+            m.setViewName("error");
+            m.addObject("errorMessage", message);
+        }else{
+            ErrorMessage message = new ErrorMessage(
+                    items[items.length-1].substring(0, 1).toLowerCase() + items[items.length-1].substring(1),
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    new Date(),
+                    ex.getMessage(),
+                    request.getDescription(true),
+                    ex.getStackTrace().toString(),
+                    ex.getLocalizedMessage(),
+                    ex.getCause().toString());
+
+            errorMessageRepository.saveAndFlush(message);
+            m.setViewName("error");
+            m.addObject("errorMessage", message);
+        }
+
         return m;
     }
 }
